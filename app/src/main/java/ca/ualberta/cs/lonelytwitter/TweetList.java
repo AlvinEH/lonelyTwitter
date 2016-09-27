c74914b89301d253041b447c034b8d438babf1f9
AlvinEH <aehuang@ualberta.ca>
2016-09-27 17:43:26 -0600

    Implement and Test add

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -0,0 +1,21 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by alvin on 9/27/2016.
+ */
+public class TweetList {
+    List<Tweet> tweets = new ArrayList<Tweet>();
+    public void add(Tweet tweet) {
+        tweets.add(tweet);
+    }
+    public boolean hasTweet(Tweet tweet) {
+        return tweets.contains(tweet);
+    }
+
+    public void getTweet(int i) {
+        return tweets.get(i);
+    }
+}
